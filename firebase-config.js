f9816cabc3035c89e395cebc2d82d370ee5a0209
anujthakur-10 <anuj10122004@gmail.com>
2025-05-18 18:11:53 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBduirdtq8s5FSi0ujPvzEva7cajvBbMko",
-    authDomain: "my-ai-app-bb622.firebaseapp.com",
-    projectId: "my-ai-app-bb622",
-    storageBucket: "my-ai-app-bb622.firebasestorage.app",
-    messagingSenderId: "849466789539",
-    appId: "1:849466789539:web:96e9b690d2b0ebcbc0b916"
+    apiKey: "your-api-key-here",
+    authDomain: "your-auth-domain",
+    projectId: "your-project-id",
+    storageBucket: "your-bucket-id",
+    messagingSenderId: "your-messagingSenderId",
+    appId: "your-app-id"
   };
   
   firebase.initializeApp(firebaseConfig);
